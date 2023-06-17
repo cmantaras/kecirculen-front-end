6a21dadd8c87c196bfe9e480427e71ffc467c30c
joseamila <josea.miladelaroca@gmail.com>
2023-06-17 18:45:45 -0300

    Added title and text components in order to have text styles standards

---
+++ src/components/Texts/text.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
+
+<template>
+  <p class="textStyle">
+    {{ text }}
+  </p>
+</template>
+
+<style>
+.textStyle {
+  font-size: 16px;
+  font-family: Poppins;
+  font-weight: 400;
+  line-height: 24px;
+}
+</style>
---
+++ src/components/Texts/title.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+
+<template>
+  <h1 class="titleStyle">
+    {{ title }}
+  </h1>
+</template>
+
+<style>
+.titleStyle {
+  font-size: 26px;
+  font-family: Poppins;
+  font-weight: 400;
+  line-height: 39px;
+  color: #716e6e;
+}
+</style>
